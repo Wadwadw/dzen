76f43dfdced230ef76f180252a5986e040ed5b4a
Leshchenko Vadym <leshchenko.vadym.n@gmail.com>
2023-02-21 16:46:07 +0200

    feat: added new model Comment fix: refactored .gitignore

--- comment_app/comment_app/settings.py
+++ comment_app/comment_app/settings.py
@@ -37,6 +37,13 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # installed apps
+    'mptt',
+    'captcha',
+
+    # my apps
+    'main_app'
 ]
 
 MIDDLEWARE = [
---
+++ comment_app/main_app/models.py
@@ -0,0 +1,21 @@
+from django.db import models
+from mptt.models import MPTTModel, TreeForeignKey
+from captcha.fields import CaptchaField
+
+
+class Comment(MPTTModel):
+    user_name = models.CharField(max_length=255)
+    email = models.EmailField()
+    home_page = models.URLField()
+    # captcha = CaptchaField()
+    text = models.TextField()
+    parent = TreeForeignKey(
+        'self',
+        on_delete=models.CASCADE,
+        null=True,
+        blank=True,
+        related_name='children'
+    )
+
+    class MPTTMeta:
+        order_insertion_by = ['name']
